6e7a42d0b52829f4b4adb24125ae5610cb35df50
Jafar Abdi <cafer.abdi@gmail.com>
2021-07-24 19:12:42 +0300

    Fix path to templates directory

--- scripts/apprun.sh
+++ scripts/apprun.sh
@@ -5,4 +5,4 @@ set -e
 APPDIR="$(dirname "$(readlink -e "$0")")"
 
 export LD_LIBRARY_PATH="${APPDIR}/usr/lib/:${APPDIR}/usr/lib/x86_64-linux-gnu${LD_LIBRARY_PATH+:$LD_LIBRARY_PATH}"
-exec "${APPDIR}/usr/bin/projects_generator" --templates_path=${APPDIR}/usr/share "$@"
+exec "${APPDIR}/usr/bin/projects_generator" --templates_path=${APPDIR}/usr/share/templates "$@"
